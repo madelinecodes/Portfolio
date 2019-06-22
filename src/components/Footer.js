e3baef9dd87c7167cd4b79f0bff43ac6d6a7568d
Madeline Reeves <missy11217@gmail.com>
2019-06-22 22:24:34 +0100

    Fixing Favicon issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import githubMark from '../assets/images/github-logo.svg'
+import glitchLogo from '../assets/images/glitch-logo.svg'
 // import mail from '../assets/images/mail.png'
 
 
@@ -11,7 +12,7 @@ class Footer extends React.Component {
                     <ul className="icons">
                         <li><span role="img" aria-label="github"><a href="https://github.com/madelinecodes" className="icon" ><img src={githubMark} alt="github" /></a></span></li>
                         <li><span role="img" aria-label="mail"><a href="madelinemhealey@gmail.com" className="icon"><span className="icon fas fa-envelope"></span></a></span></li>
-                        <li><span role="img" aria-label="glitch"><a href="https://glitch.com/@madelinecodes" className="icon"><input type="image" src='https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Flogo-day.svg' alt="Glitch" /></a></span></li>
+                        <li><span role="img" aria-label="glitch"><a href="https://glitch.com/@madelinecodes" className="icon"><input type="image" src={glitchLogo} alt="Glitch" /></a></span></li>
                     </ul>
                 </div>
             </div>
